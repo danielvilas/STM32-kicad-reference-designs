9150096281c9fe3c96402608a095d5a61dfb398f
Daniel Vilas <daniel.vilas.perulan@everis.com>
2021-08-23 12:12:34 +0200

    Kibot Split on files

--- STM32F0/STM32F030/STM32F030K6T6TR_Devel/runkibot.sh
+++ STM32F0/STM32F030/STM32F030K6T6TR_Devel/runkibot.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-kibot -c ../../../kibot/full.kibot.yaml -b STM32F030K6T6TR_Devel.kicad_pcb -e STM32F030K6T6TR_Devel.sch -d doc $@
+
+BOARD=STM32F030K6T6TR_Devel
+rm -rf doc
+
+#kibot -c ../../../kibot/00_erc_drc.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+kibot -c ../../../kibot/01_pdf_doc.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+kibot -c ../../../kibot/02_build_doc.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+kibot -c ../../../kibot/03_graphic_doc.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+kibot -c ../../../kibot/04_fab_board.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+kibot -c ../../../kibot/05_fab_pcba.kibot.yaml -b $BOARD.kicad_pcb -e $BOARD.sch -d doc $@
+
+rm -f doc/*.ogv
---
+++ kibot/README.md
@@ -0,0 +1,16 @@
+# Kibot Scripts
+This folder contains the kibot scripts in order to document the projects. So, all projects have the same documentation.
+
+
+## Install
+
+```
+sudo apt install xvfb recordmydesktop xdotool librsvg2-bin imagemagick
+curl -OL https://github.com/INTI-CMNB/InteractiveHtmlBom/releases/download/v2.3.4-2/interactivehtmlbom.inti-cmnb_2.3.4-2_all.deb 
+sudo apt install ./interactivehtmlbom.inti-cmnb_2.3.4-2_all.deb
+rm ./interactivehtmlbom.inti-cmnb_2.3.4-2_all.deb
+
+pip3 install kibot --no-compile
+pip3 install kikit pcbdraw
+
+```
